3265332f426566daae295aa01b05801d93901f77
Samer99Najjar <80600556+Samer99Najjar@users.noreply.github.com>
2023-05-01 19:56:27 +0300

    Employee Class is done

--- index.ts
+++ index.ts
@@ -6,11 +6,26 @@ class Person{
         this.age= age_;
     }
     greet(){
-        
         console.log("Hello i am ," +this.name);
-
     }
-    getAge(){
+    getAge(){   
         return this.age;
     }
 }
+class Employee extends Person{
+    id: number;
+    salary: number;
+    constructor(name_: string , age_: number, id_: number, salary_:number){
+        super(name_,age_);
+        this.id=id_;
+        this.salary=salary_;
+    }
+    work(){
+        console.log(`Hello, my name is ${this.name} and my id is ${this.id} I am ${this.age} years old .`);
+    }
+    getSalary(){
+        return this.salary;
+    }
+
+}
+
